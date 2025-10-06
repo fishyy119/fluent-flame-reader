f5adc8c7e848704d8603514eebdd13297fd5991e
CrystalSplitter <crystal@crystalwobsite.gay>
2025-10-05 21:31:54 -0700

    Update nixpkgs pinning

--- nix/default.nix
+++ nix/default.nix
@@ -1,7 +1,7 @@
 let
   nixpkgsTarball = fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/tarball/85dbfc7aaf52ecb755f87e577ddbe6dbbdbc1054";
-    sha256 = "sha256-iAcj9T/Y+3DBy2J0N+yF9XQQQ8IEb5swLFzs23CdP88=";
+    url = "https://github.com/NixOS/nixpkgs/tarball/7df7ff7d8e00218376575f0acdcc5d66741351ee";
+    sha256 = "sha256-gTrEEp5gEspIcCOx9PD8kMaF1iEmfBcTbO0Jag2QhQs=";
   };
   pkgs = import nixpkgsTarball {
     config = { };
