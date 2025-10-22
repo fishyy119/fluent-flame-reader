4b099bcb89670cb8b99c0492734c4d7d9d4ae8cb
CrystalSplitter <crystal@crystalwobsite.gay>
2025-10-21 20:31:12 -0700

    Update flake and nixpkgs

--- nix/default.nix
+++ nix/default.nix
@@ -1,7 +1,7 @@
 let
   nixpkgsTarball = fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/tarball/7df7ff7d8e00218376575f0acdcc5d66741351ee";
-    sha256 = "sha256-gTrEEp5gEspIcCOx9PD8kMaF1iEmfBcTbO0Jag2QhQs=";
+    url = "https://github.com/NixOS/nixpkgs/tarball/5e2a59a5b1a82f89f2c7e598302a9cacebb72a67";
+    sha256 = "sha256-K5Osef2qexezUfs0alLvZ7nQFTGS9DL2oTVsIXsqLgs=";
   };
   pkgs = import nixpkgsTarball {
     config = { };
