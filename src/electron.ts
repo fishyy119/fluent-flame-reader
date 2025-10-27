297f0621fcd1e3b874a7fa9ee194ce0cb52a5d1c
CrystalSplitter <crystal@crystalwobsite.gay>
2025-10-26 18:51:09 -0700

    Update appExec naming

--- src/electron.ts
+++ src/electron.ts
@@ -13,7 +13,7 @@ if (!process.mas) {
 
 if (!app.isPackaged) app.setAppUserModelId(process.execPath)
 else if (process.platform === "win32")
-    app.setAppUserModelId("me.hyliu.fluentreader")
+    app.setAppUserModelId("org.fluentflame.fluentflamereader")
 
 let restarting = false
 
