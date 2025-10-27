4ceb7eb4d5cacebc8837ad24e1fec66d61fe972a
CrystalSplitter <crystal@crystalwobsite.gay>
2025-10-26 18:28:38 -0700

    Update nix flake description

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "FluentReader: An RSS Reader Written in React";
+  description = "Fluentflame Reader: A Sleek Desktop RSS Reader";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
