33bee93879bc99904c57ccfcd335f33bc6eda8d6
CrystalSplitter <crystal@crystalwobsite.gay>
2025-10-26 18:29:10 -0700

    Rename exports

--- src/scripts/models/group.ts
+++ src/scripts/models/group.ts
@@ -325,12 +325,12 @@ export function exportOPML(): AppThunk {
             { name: intl.get("sources.opmlFile"), extensions: ["opml"] },
         ]
         window.utils
-            .showSaveDialog(filters, "*/Fluent_Reader_Export.opml")
+            .showSaveDialog(filters, "*/fluentflame_reader_export.opml")
             .then(write => {
                 if (write) {
                     let state = getState()
                     let xml = new DOMParser().parseFromString(
-                        '<?xml version="1.0" encoding="UTF-8"?><opml version="1.0"><head><title>Fluent Reader Export</title></head><body></body></opml>',
+                        '<?xml version="1.0" encoding="UTF-8"?><opml version="1.0"><head><title>Fluentflame Reader Export</title></head><body></body></opml>',
                         "text/xml"
                     )
                     let body = xml.getElementsByTagName("body")[0]
--- src/scripts/settings.ts
+++ src/scripts/settings.ts
@@ -162,7 +162,7 @@ export async function exportAll() {
     const filters = [{ name: intl.get("app.frData"), extensions: ["frdata"] }]
     const write = await window.utils.showSaveDialog(
         filters,
-        "*/Fluent_Reader_Backup.frdata",
+        "*/fluentflame_reader_backup.frdata",
     )
     if (write) {
         let output = window.settings.getAll()
