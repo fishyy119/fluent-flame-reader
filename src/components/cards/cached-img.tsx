6c8dcd5878f8b668514b987f35f7723031224962
GriddleharkBrainrot <j.voronik@proton.me>
2025-11-03 03:30:18 +0200

    Fix image loading

--- src/components/cards/cached-img.tsx
+++ src/components/cards/cached-img.tsx
@@ -5,60 +5,67 @@ type ImgProps = {
     className?: string
 }
 
-const cache = new Map<string, HTMLImageElement>()
+class CachedImg extends React.Component<ImgProps> {
+    private static readonly _cache = new Map<string, HTMLImageElement>()
+    private readonly _canvasRef = React.createRef<HTMLCanvasElement>()
+    private _img: HTMLImageElement | null = null
 
-function draw(
-    canvasRef: React.MutableRefObject<HTMLCanvasElement>,
-    img: HTMLImageElement,
-) {
-    const canvas = canvasRef.current
-    if (!canvas) {
-        setTimeout(() => draw(canvasRef, img), 100)
-        return
+    constructor(props: ImgProps) {
+        super(props)
     }
-    const maxWidth = 256
-    const maxHeight = 256
-    if (img.width > maxWidth || img.height > maxHeight) {
-        const scaleFactor = Math.max(
-            img.width / maxWidth,
-            img.height / maxHeight,
+
+    private draw() {
+        const canvas = this._canvasRef.current
+        if (!canvas || !this._img) {
+            return
+        }
+        const maxWidth = 256
+        const maxHeight = 256
+        if (this._img.width > maxWidth || this._img.height > maxHeight) {
+            const scaleFactor = Math.max(
+                this._img.width / maxWidth,
+                this._img.height / maxHeight,
+            )
+            canvas.width = this._img.width / scaleFactor
+            canvas.height = this._img.height / scaleFactor
+        } else {
+            canvas.width = this._img.width
+            canvas.height = this._img.height
+        }
+        const ctx = canvas.getContext("2d")
+        ctx.drawImage(
+            this._img,
+            0,
+            0,
+            this._img.width,
+            this._img.height,
+            0,
+            0,
+            canvas.width,
+            canvas.height,
         )
-        canvas.width = img.width / scaleFactor
-        canvas.height = img.height / scaleFactor
-    } else {
-        canvas.width = img.width
-        canvas.height = img.height
     }
-    const ctx = canvas.getContext("2d")
-    ctx.drawImage(
-        img,
-        0,
-        0,
-        img.width,
-        img.height,
-        0,
-        0,
-        canvas.width,
-        canvas.height,
-    )
-}
 
-const img = (props: ImgProps) => {
-    const canvasRef = React.useRef<HTMLCanvasElement>()
-    const canvas = <canvas className={props.className} ref={canvasRef}></canvas>
-    if (cache.has(props.src)) {
-        const img = cache.get(props.src)
-        if (img.naturalWidth > 0 && img.naturalHeight > 0) draw(canvasRef, img)
-        else img.addEventListener("loadeddata", () => draw(canvasRef, img))
+    render(): React.ReactNode {
+        const canvas = (
+            <canvas
+                className={this.props.className}
+                ref={this._canvasRef}></canvas>
+        )
+        if (CachedImg._cache.has(this.props.src)) {
+            this._img = CachedImg._cache.get(this.props.src)
+        } else {
+            this._img = new Image()
+            CachedImg._cache.set(this.props.src, this._img)
+            this._img.loading = "eager"
+            this._img.src = this.props.src
+        }
+        this._img
+            .decode()
+            .then(() => this.draw())
+            .catch(() => this.forceUpdate())
         return canvas
     }
-    const img = new Image()
-    img.src = props.src
-    img.loading = "eager"
-    cache.set(props.src, img)
-    img.addEventListener("loadeddata", () => draw(canvasRef, img))
-    return canvas
 }
 
-const CachedImg: React.FunctionComponent<ImgProps> = props => img(props)
 export default CachedImg
