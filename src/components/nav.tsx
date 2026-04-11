60874a6058241b07e774b880866e9baf1a8e48b6
CrystalSplitter <crystal@crystalwobsite.gay>
2026-04-11 14:39:04 -0700

    Remove nav-container
    This refactor reworks the Nav object to use more modern
    react-redux APIs. This lets us remove nav-container entirely,
    which is obselete with new hooks.
    
    Further cleanup is possible, though this seems to work for now.
    
    This is based on the upstream Fluent Reader change `feb6429`,
    which seems buggy and also violates our contribution guidelines.
    However, the idea of modernising this is solid, so I have
    re-implemented it from scratch.

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,253 +1,263 @@
 import * as React from "react";
+import { useSelector, useDispatch } from "react-redux";
 import intl from "react-intl-universal";
 import { Icon } from "@fluentui/react/lib/Icon";
-import { AppState } from "../scripts/models/app";
 import { ProgressIndicator, IObjectWithKey } from "@fluentui/react";
-import { getWindowBreakpoint } from "../scripts/utils";
+
+import {
+    openMarkAllMenu,
+    openViewMenu,
+    toggleLogMenu,
+    toggleMenu,
+    toggleSettings,
+} from "../scripts/models/app";
+import { RootState } from "../scripts/reducer";
+import { ViewType } from "../schema-types";
 import { WindowStateListenerType } from "../schema-types";
+import { fetchItems } from "../scripts/models/item";
+import { toggleSearch } from "../scripts/models/page";
 
-type NavProps = {
-    state: AppState;
-    itemShown: boolean;
-    menu: () => void;
-    search: () => void;
-    markAllRead: () => void;
-    fetch: () => void;
-    logs: () => void;
-    views: () => void;
-    settings: () => void;
-};
+function close() {
+    window.utils.closeWindow();
+}
 
-type NavState = {
-    maximized: boolean;
-};
+function setBodyFocusState(focused: boolean) {
+    if (focused) document.body.classList.remove("blur");
+    else document.body.classList.add("blur");
+}
 
-class Nav extends React.Component<NavProps, NavState> {
-    constructor(props) {
-        super(props);
-        this.setBodyFocusState(window.utils.isFocused());
-        this.setBodyFullscreenState(window.utils.isFullscreen());
-        window.utils.addWindowStateListener(this.windowStateListener);
-        this.state = {
-            maximized: window.utils.isMaximized(),
-        };
-    }
+function setBodyFullscreenState(fullscreen: boolean) {
+    if (fullscreen) document.body.classList.remove("not-fullscreen");
+    else document.body.classList.add("not-fullscreen");
+}
 
-    setBodyFocusState = (focused: boolean) => {
-        if (focused) document.body.classList.remove("blur");
-        else document.body.classList.add("blur");
+// TODO: Selectors should be defined inside slice files rather than
+// near components, see https://redux.js.org/usage/deriving-data-selectors
+function selectAppState(state: RootState) {
+    return state.app;
+}
+
+function selectItemShown(state: RootState) {
+    return (
+        state.page.itemId && state.page.viewConfig.currentView !== ViewType.List
+    );
+}
+
+// Component --------------------------------------------------------------------------------------
+
+/** The top navigation bar, containing buttons */
+function Nav() {
+    const dispatch = useDispatch();
+    const [maximized, setMaximized] = React.useState(
+        window.utils.isMaximized(),
+    );
+    const state = useSelector(selectAppState);
+    const itemShown = useSelector(selectItemShown);
+
+    const menu = () => dispatch(toggleMenu());
+    const search = () => dispatch(toggleSearch());
+    const markAllRead = () => dispatch(openMarkAllMenu());
+    const logs = () => dispatch(toggleLogMenu());
+    const settings = () => dispatch(toggleSettings());
+    const views = () => dispatch(openViewMenu());
+
+    const minimize = () => {
+        window.utils.minimizeWindow();
+        setMaximized(false);
     };
 
-    setBodyFullscreenState = (fullscreen: boolean) => {
-        if (fullscreen) document.body.classList.remove("not-fullscreen");
-        else document.body.classList.add("not-fullscreen");
+    const maximize = () => {
+        window.utils.maximizeWindow();
+        setMaximized(true);
     };
 
-    windowStateListener = (type: WindowStateListenerType, state: boolean) => {
-        switch (type) {
-            case WindowStateListenerType.Maximized:
-                this.setState({ maximized: state });
-                break;
-            case WindowStateListenerType.Fullscreen:
-                this.setBodyFullscreenState(state);
-                break;
-            case WindowStateListenerType.Focused:
-                this.setBodyFocusState(state);
-                break;
-        }
+    const canFetch = () =>
+        state.sourceInit &&
+        state.feedInit &&
+        !state.syncing &&
+        !state.fetchingItems;
+
+    const feedFetch = () => {
+        if (canFetch()) dispatch(fetchItems());
     };
 
-    navShortcutsHandler = (e: KeyboardEvent | IObjectWithKey) => {
-        if (!this.props.state.settings.display) {
+    const fetching = () => (!canFetch() ? " fetching" : "");
+
+    const getClassNames = () => {
+        const classNames = new Array<string>();
+        classNames.push("top-nav");
+        if (state.settings.display) classNames.push("hide-btns");
+        if (state.menu) classNames.push("menu-on");
+        if (itemShown) classNames.push("item-on");
+        return classNames.join(" ");
+    };
+
+    const navShortcutsHandler = (e: KeyboardEvent | IObjectWithKey) => {
+        if (!state.settings.display) {
             switch (e.key) {
                 case "F1":
-                    this.props.menu();
-                    break;
+                    menu();
+                    return;
                 case "F2":
-                    this.props.search();
-                    break;
+                    search();
+                    return;
                 case "F5":
-                    this.fetch();
-                    break;
+                    feedFetch();
+                    return;
                 case "F6":
-                    this.props.markAllRead();
-                    break;
+                    markAllRead();
+                    return;
                 case "F7":
-                    if (!this.props.itemShown) this.props.logs();
-                    break;
+                    if (!itemShown) logs();
+                    return;
                 case "F8":
-                    if (!this.props.itemShown) this.props.views();
-                    break;
+                    if (!itemShown) views();
+                    return;
                 case "F9":
-                    if (!this.props.itemShown) this.props.settings();
-                    break;
+                    if (!itemShown) settings();
+                    return;
             }
         }
     };
 
-    componentDidMount() {
-        document.addEventListener("keydown", this.navShortcutsHandler);
-        if (window.utils.platform === "darwin")
-            window.utils.addTouchBarEventsListener(this.navShortcutsHandler);
-    }
-    componentWillUnmount() {
-        document.removeEventListener("keydown", this.navShortcutsHandler);
-    }
-
-    minimize = () => {
-        window.utils.minimizeWindow();
-    };
-    maximize = () => {
-        window.utils.maximizeWindow();
-        this.setState({ maximized: !this.state.maximized });
-    };
-    close = () => {
-        window.utils.closeWindow();
-    };
-
-    canFetch = () =>
-        this.props.state.sourceInit &&
-        this.props.state.feedInit &&
-        !this.props.state.syncing &&
-        !this.props.state.fetchingItems;
-    fetching = () => (!this.canFetch() ? " fetching" : "");
-    getClassNames = () => {
-        const classNames = new Array<string>();
-        classNames.push("top-nav");
-        if (this.props.state.settings.display) classNames.push("hide-btns");
-        if (this.props.state.menu) classNames.push("menu-on");
-        if (this.props.itemShown) classNames.push("item-on");
-        return classNames.join(" ");
-    };
-
-    fetch = () => {
-        if (this.canFetch()) this.props.fetch();
-    };
-
-    views = () => {
-        if (this.props.state.contextMenu.event !== "#view-toggle") {
-            this.props.views();
-        }
-    };
-
-    getProgress = () => {
-        return this.props.state.fetchingTotal > 0
-            ? this.props.state.fetchingProgress / this.props.state.fetchingTotal
+    const getProgress = () => {
+        return state.fetchingTotal > 0
+            ? state.fetchingProgress / state.fetchingTotal
             : null;
     };
 
-    render() {
-        return (
-            <nav className={this.getClassNames()}>
-                <div className="btn-group">
-                    <a
-                        className="btn hide-wide"
-                        title={intl.get("nav.menu")}
-                        onClick={this.props.menu}>
-                        <Icon
-                            iconName={
-                                window.utils.platform === "darwin"
-                                    ? "SidePanel"
-                                    : "GlobalNavButton"
-                            }
-                        />
-                    </a>
-                </div>
-                <span className="title hide-on-tiny">
-                    {this.props.state.title}
-                </span>
-                <div className="btn-group corner-btn-group">
-                    <a
-                        className={"btn" + this.fetching()}
-                        onClick={this.fetch}
-                        title={intl.get("nav.refresh")}>
-                        <Icon iconName="Refresh" />
-                    </a>
-                    <a
-                        className="btn hide-on-tiny"
-                        id="mark-all-toggle"
-                        onClick={this.props.markAllRead}
-                        title={intl.get("nav.markAllRead")}
-                        onMouseDown={(e) => {
-                            if (
-                                this.props.state.contextMenu.event ===
-                                "#mark-all-toggle"
-                            )
-                                e.stopPropagation();
-                        }}>
-                        <Icon iconName="InboxCheck" />
-                    </a>
-                    <a
-                        className="btn"
-                        id="log-toggle"
-                        title={intl.get("nav.notifications")}
-                        onClick={this.props.logs}>
-                        {this.props.state.logMenu.notify ? (
-                            <Icon iconName="RingerSolid" />
-                        ) : (
-                            <Icon iconName="Ringer" />
-                        )}
-                    </a>
-                    <a
-                        className="btn hide-on-tiny"
-                        id="view-toggle"
-                        title={intl.get("nav.view")}
-                        onClick={this.props.views}
-                        onMouseDown={(e) => {
-                            if (
-                                this.props.state.contextMenu.event ===
-                                "#view-toggle"
-                            )
-                                e.stopPropagation();
-                        }}>
-                        <Icon iconName="View" />
-                    </a>
-                    <a
-                        className="btn"
-                        title={intl.get("nav.settings")}
-                        onClick={this.props.settings}>
-                        <Icon iconName="Settings" />
-                    </a>
-                    <span className="seperator"></span>
-                    <a
-                        className="btn system"
-                        title={intl.get("nav.minimize")}
-                        onClick={this.minimize}
-                        style={{ fontSize: 12 }}>
-                        <Icon iconName="Remove" />
-                    </a>
-                    <a
-                        className="btn system"
-                        title={intl.get("nav.maximize")}
-                        onClick={this.maximize}>
-                        {this.state.maximized ? (
-                            <Icon
-                                iconName="ChromeRestore"
-                                style={{ fontSize: 11 }}
-                            />
-                        ) : (
-                            <Icon
-                                iconName="Checkbox"
-                                style={{ fontSize: 10 }}
-                            />
-                        )}
-                    </a>
-                    <a
-                        className="btn system close"
-                        title={intl.get("close")}
-                        onClick={this.close}>
-                        <Icon iconName="Cancel" />
-                    </a>
-                </div>
-                {!this.canFetch() && (
-                    <ProgressIndicator
-                        className="progress"
-                        percentComplete={this.getProgress()}
+    // componentDidMount replacement.
+    React.useEffect(() => {
+        setBodyFocusState(window.utils.isFocused());
+        setBodyFullscreenState(window.utils.isFullscreen());
+        const windowStateListener = (
+            type: WindowStateListenerType,
+            value: boolean,
+        ) => {
+            switch (type) {
+                case WindowStateListenerType.Maximized:
+                    setMaximized(value);
+                    break;
+                case WindowStateListenerType.Fullscreen:
+                    setBodyFullscreenState(value);
+                    break;
+                case WindowStateListenerType.Focused:
+                    setBodyFocusState(value);
+                    break;
+            }
+        };
+        window.utils.addWindowStateListener(windowStateListener);
+
+        document.addEventListener("keydown", navShortcutsHandler);
+        if (window.utils.platform === "darwin")
+            window.utils.addTouchBarEventsListener(navShortcutsHandler);
+    }, []);
+
+    // componentWillUnmount replacement.
+    React.useEffect(() => {
+        return () =>
+            document.removeEventListener("keydown", navShortcutsHandler);
+    }, []);
+
+    return (
+        <nav className={getClassNames()}>
+            <div className="btn-group">
+                <a
+                    className="btn hide-wide"
+                    title={intl.get("nav.menu")}
+                    onClick={menu}>
+                    <Icon
+                        iconName={
+                            window.utils.platform === "darwin"
+                                ? "SidePanel"
+                                : "GlobalNavButton"
+                        }
                     />
-                )}
-            </nav>
-        );
-    }
+                </a>
+            </div>
+            <span className="title hide-on-tiny">{state.title}</span>
+            <div className="btn-group corner-btn-group">
+                <a
+                    className={"btn" + fetching()}
+                    onClick={feedFetch}
+                    title={intl.get("nav.refresh")}>
+                    <Icon iconName="Refresh" />
+                </a>
+                <a
+                    className="btn hide-on-tiny"
+                    id="mark-all-toggle"
+                    onClick={markAllRead}
+                    title={intl.get("nav.markAllRead")}
+                    onMouseDown={(e) => {
+                        if (state.contextMenu.event === "#mark-all-toggle")
+                            e.stopPropagation();
+                    }}>
+                    <Icon iconName="InboxCheck" />
+                </a>
+                <a
+                    className="btn"
+                    id="log-toggle"
+                    title={intl.get("nav.notifications")}
+                    onClick={logs}>
+                    {state.logMenu.notify ? (
+                        <Icon iconName="RingerSolid" />
+                    ) : (
+                        <Icon iconName="Ringer" />
+                    )}
+                </a>
+                <a
+                    className="btn hide-on-tiny"
+                    id="view-toggle"
+                    title={intl.get("nav.view")}
+                    onClick={views}
+                    onMouseDown={(e) => {
+                        if (state.contextMenu.event === "#view-toggle")
+                            e.stopPropagation();
+                    }}>
+                    <Icon iconName="View" />
+                </a>
+                <a
+                    className="btn"
+                    title={intl.get("nav.settings")}
+                    onClick={settings}>
+                    <Icon iconName="Settings" />
+                </a>
+                <span className="seperator"></span>
+                <a
+                    className="btn system"
+                    title={intl.get("nav.minimize")}
+                    onClick={minimize}
+                    style={{ fontSize: 12 }}>
+                    <Icon iconName="Remove" />
+                </a>
+                <a
+                    className="btn system"
+                    title={intl.get("nav.maximize")}
+                    onClick={maximize}>
+                    {maximized ? (
+                        <Icon
+                            iconName="ChromeRestore"
+                            style={{ fontSize: 11 }}
+                        />
+                    ) : (
+                        <Icon iconName="Checkbox" style={{ fontSize: 10 }} />
+                    )}
+                </a>
+                <a
+                    className="btn system close"
+                    title={intl.get("close")}
+                    onClick={close}>
+                    <Icon iconName="Cancel" />
+                </a>
+            </div>
+            {!canFetch() && (
+                <ProgressIndicator
+                    className="progress"
+                    percentComplete={getProgress()}
+                />
+            )}
+        </nav>
+    );
 }
 
 export default Nav;
