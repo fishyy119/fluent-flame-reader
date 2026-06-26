1cbc495a6d3d51da4e3d2712db1c5f784d646340
CrystalSplitter <crystal@crystalwobsite.gay>
2026-06-26 00:22:22 -0700

    Update nix flake.lock

--- nix/default.nix
+++ nix/default.nix
@@ -1,7 +1,7 @@
 let
   nixpkgsTarball = fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/tarball/4c1018dae018162ec878d42fec712642d214fdfa";
-    sha256 = "sha256-ar3rofg+awPB8QXDaFJhJ2jJhu+KqN/PRCXeyuXR76E=";
+    url = "https://github.com/NixOS/nixpkgs/tarball/567a49d1913ce81ac6e9582e3553dd90a955875f";
+    sha256 = "sha256-lrp67w8AulE9Ks53n27I45ADSzbOCn4H+CNW1Ck8B+8=";
   };
   pkgs = import nixpkgsTarball {
     config = { };
